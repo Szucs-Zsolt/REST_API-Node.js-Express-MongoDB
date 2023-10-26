bbdc9168e252ee1211e815f6fc334776734c5684
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-10-26 09:54:35 +0200

    POST fogadása, kapott értékek visszaküldése.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const kerdoivRoute = require("./routes/kerdoiv");
 const PORT = process.env.SERVER_PORT || 12345;
 
 const app = express();
-
+app.use(express.json());
 app.use("/kerdoiv", kerdoivRoute);
 
 app.listen(PORT, () => {
--- routes/kerdoiv.js
+++ routes/kerdoiv.js
@@ -5,15 +5,28 @@ const router = express.Router();
 //@route POST /kerdoiv/kitoltes
 //@access public
 //TODO Beküldött kérdőív lementése az adatbázisba.
-router.post("/bekuldes", (req, res)=>{
-    res.status(200).send("TODO Beküldött kérdőív lementése az adatbázisba.");
+router.post("/bekuldes", (req, res) => {
+    let elegedettseg = req.body.elegedettseg;
+
+    // Hiba: érvénytelen az adat
+    if (!elegedettseg || elegedettseg<"1" || elegedettseg >"5") {
+        res.status(400).json({
+            "message": "Hiányzó, vagy hibás adat (érvényes érték: 1-5 között))!"
+        });
+    }
+    // OK: egyelőre csak visszaküldjük a kapott adatot
+    res.status(200).json({
+        "message": "TODO Beküldött kérdőív lementése az adatbázisba.",
+        "elegedettseg": elegedettseg
+    });
 });
+
 
 //@desc Kitöltött kérdőív beküldése
 //@route GET /kerdoiv/osszesites
 //@access public
 //TODO Adatbázisban lévő kérdőívek összesítése.
-router.get("/osszesites", (req, res)=>{
+router.get("/osszesites", (req, res) => {
     res.status(200).send("TODO Adatbázisban lévő kérdőívek összesítése.");
 });
 
